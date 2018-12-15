b5822f0f88a9a1b2caa37c06b797c9a3b9693a78
SteeleTyler <tsteele02@wildcats.jwu.edu>
2018-12-15 10:28:16 -0500

    Basic Vue functionality working for display, placeholder style

---
+++ src/components/BodyDisplay.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+  <ul>
+    <li  v-for="(value, index) in lines" :key="index">
+      {{ value }}
+    </li>
+  </ul>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'BodyDisplay',
+    props: {
+      lines: Array,
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+  }
+</style>
