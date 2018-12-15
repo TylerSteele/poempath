018e01b90b8d11e3961591b50b9d546978a26fbe
SteeleTyler <tsteele02@wildcats.jwu.edu>
2018-12-15 10:28:16 -0500

    Get basic Vue functionality working for display, placeholder style

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <PoemDisplay title="In a Station of the Metro" author="Ezra Pound" body="The apparition of these faces in the crowd:\nPetals on a wet, black bough ."/>
+    <div>
+      <p id="mainHeading">>> Poetically Sense >></p>
+      <p id="subHeading">Poetry you'll enjoy. </p>
+    </div>
+    <PoemDisplay v-bind:title="title" v-bind:body="body" v-bind:author="author"/>
   </div>
 </template>
 
 <script>
-import PoemDisplay from './components/PoemDisplay.vue'
+  import PoemDisplay from './components/PoemDisplay.vue'
 
-export default {
-  name: 'app',
-  components: {
-    PoemDisplay
+  export default {
+    name: 'app',
+    components: {
+      PoemDisplay
+    },
+    data: function () {
+      return {
+        title: "In a Station of the Metro",
+        author: "Ezra Pound",
+        body: ["The apparition of these faces in the crowd:", "Petals on a wet, black bough."]
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    font-family: 'Roboto', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #6d8fb3;
+    background-color: #121212;
+    margin-top: 60px;
+  }
+  #mainHeading {
+    font-size: 2em;
+    text-underline: aqua;
+    text-decoration: underline overline;
+    text-align: center;
+    margin: .5em;
+
+  }
+  #subHeading {
+    font-style: italic;
+    font-size: 1.5em;
+    text-align: center;
+    margin: 0;
+  }
 </style>
--- src/components/PoemDisplay.vue
+++ src/components/PoemDisplay.vue
@@ -1,39 +1,36 @@
 <template>
-  <div class="hello">
-    <h1>Poetically Sense</h1>
-    <h3>Poetry you'll enjoy. </h3>
+  <div>
+
     <h4>{{ title }}</h4>
     <h5>{{ author }}</h5>
-    <p>{{ body }}</p>
+    <BodyDisplay v-bind:lines="body"></BodyDisplay>
     <!-- TODO: create component for the body of each poem to render each line within <pre> tags-->
   </div>
 </template>
 
 <script>
-export default {
-  name: 'PoemDisplay',
-  props: {
-    title: String,
-    body: String,
-    author: String,
+  import BodyDisplay from './BodyDisplay.vue'
+
+  export default {
+    name: 'PoemDisplay',
+    props: {
+      title: String,
+      body: Array,
+      author: String,
+    },
+    components: {
+      BodyDisplay
+    }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  h3 {
+    margin: 40px 0 0;
+  }
+  div {
+    margin-left: 20%;
+    margin-right: 20%;
+  }
 </style>
