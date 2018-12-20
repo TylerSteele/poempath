9588d743be50eaa0beee4a5235475fa264380a72
SteeleTyler <tsteele02@wildcats.jwu.edu>
2018-12-20 14:59:06 -0500

    Switch to Quasar framework, basic primary view

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import './styles/quasar.styl'
+import 'quasar-extras/animate'
+import 'quasar-extras/roboto-font'
+import 'quasar-extras/material-icons'
+import Quasar from 'quasar'
+
+Vue.use(Quasar, {
+  config: {}
+ })
+
 Vue.config.productionTip = false
 
 new Vue({
