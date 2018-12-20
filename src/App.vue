27ac2c0b9ad23f4feabb6abf651152c43955cd8d
SteeleTyler <tsteele02@wildcats.jwu.edu>
2018-12-20 14:59:06 -0500

    Switch to Quasar framework, basic primary view

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,82 @@
 <template>
-  <div id="app">
-    <div>
-      <p id="mainHeading">>> Poetically Sense >></p>
-      <p id="subHeading">Poetry you'll enjoy. </p>
-    </div>
-    <PoemDisplay v-bind:title="title" v-bind:body="body" v-bind:author="author"/>
-  </div>
+  <q-layout id="q-app" view="lHh Lpr lFf">
+    <q-layout-header>
+      <q-toolbar
+        color="primary"
+        :glossy="$q.theme === 'mat'"
+        :inverted="$q.theme === 'ios'"
+      >
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          aria-label="Menu"
+          icon="menu"
+        />
+
+        <q-toolbar-title>
+          Poetically Sense
+          <div slot="subtitle">Poems you know you'll enjoy</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-layout-header>
+
+    <q-layout-drawer
+      v-model="leftDrawerOpen"
+      :content-class="$q.theme === 'mat' ? 'bg-grey-2' : null"
+    >
+      <q-list
+        no-border
+        link
+        inset-delimiter
+      >
+        <q-list no-border link inset-delimiter>
+          <q-list-header>Essential Links</q-list-header>
+          <q-item @click.native="openURL('http://quasar-framework.org')">
+            <q-item-side icon="school" />
+            <q-item-main label="Docs" sublabel="quasar-framework.org"></q-item-main>
+          </q-item>
+          <q-item @click.native="openURL('https://discord.gg/5TDhbDg')">
+            <q-item-side icon="chat" />
+            <q-item-main label="Discord Chat Channel" sublabel="https://discord.gg/5TDhbDg"></q-item-main>
+          </q-item>
+          <q-item @click.native="openURL('http://forum.quasar-framework.org')">
+            <q-item-side icon="forum" />
+            <q-item-main label="Forum" sublabel="forum.quasar-framework.org"></q-item-main>
+          </q-item>
+          <q-item @click.native="openURL('https://twitter.com/quasarframework')">
+            <q-item-side icon="rss feed" />
+            <q-item-main label="Twitter" sublabel="@quasarframework"></q-item-main>
+          </q-item>
+        </q-list>
+      </q-list>
+    </q-layout-drawer>
+
+    <q-page-container>
+      <PrimaryView />
+    </q-page-container>
+  </q-layout>
 </template>
 
 <script>
-  import PoemDisplay from './components/PoemDisplay.vue'
+import { openURL } from 'quasar'
+import PrimaryView from './components/PrimaryView.vue'
 
-  export default {
-    name: 'app',
-    components: {
-      PoemDisplay
-    },
-    data: function () {
-      return {
-        title: "In a Station of the Metro",
-        author: "Ezra Pound",
-        body: ["The apparition of these faces in the crowd:", "Petals on a wet, black bough."]
-      }
+export default {
+  name: 'LayoutDefault',
+  components: {
+    PrimaryView
+  },
+  data () {
+    return {
+      leftDrawerOpen: this.$q.platform.is.desktop
     }
+  },
+  methods: {
+    openURL
   }
+}
 </script>
 
 <style>
@@ -34,19 +87,5 @@
     color: #6d8fb3;
     background-color: #121212;
     margin-top: 60px;
-  }
-  #mainHeading {
-    font-size: 2em;
-    text-underline: aqua;
-    text-decoration: underline overline;
-    text-align: center;
-    margin: .5em;
-
-  }
-  #subHeading {
-    font-style: italic;
-    font-size: 1.5em;
-    text-align: center;
-    margin: 0;
   }
 </style>
---
+++ src/components/PrimaryView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <p id="mainHeading">>> Poetically Sense >></p>
+    <p id="subHeading">Poetry you'll enjoy. </p>
+    <PoemDisplay v-bind:title="poemTitle" v-bind:body="poemBody" v-bind:author="poemAuthor"/>
+  </div>
+</template>
+
+<style>
+  #mainHeading {
+    font-size: 2em;
+    text-underline: aqua;
+    text-decoration: underline overline;
+    text-align: center;
+    margin: .5em;
+
+  }
+  #subHeading {
+    font-style: italic;
+    font-size: 1.5em;
+    text-align: center;
+    margin: 0;
+  }
+</style>
+
+<script>
+  import PoemDisplay from './PoemDisplay'
+export default {
+  name: 'PrimaryView',
+  components: {PoemDisplay},
+  data: function() {
+    return {
+      poemTitle: "Placeholder Author",
+      poemBody: ['First line', 'Second line', 'Third line'],
+      poemAuthor: 'Placeholder Author'
+    }
+
+  }
+}
+</script>
