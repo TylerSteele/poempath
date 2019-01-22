5fe25fff4f4265cb8a2a9333d4b6e69c5a450a13
SteeleTyler <tsteele02@wildcats.jwu.edu>
2019-01-21 22:20:43 -0500

    Create delete and validate routes. Validate not working yet

--- server/mongo.js
+++ server/mongo.js
@@ -1,9 +1,9 @@
-const MongoClient = require('mongodb').MongoClient
+const { MongoClient } = require('mongodb')
 const MONGO_URL = 'mongodb://localhost:27017'
 
 
 module.exports = function (app) {
-  MongoClient.connect(MONGO_URL, { useNewUrlParser: true }, (err, client) => {
+  MongoClient.connect(MONGO_URL, {useNewUrlParser: true}, (err, client) => {
     // Client returned
     app.poems = client.db('test').collection('poems')
     app.users = client.db('test').collection('users')
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,20 +1,19 @@
 const bcrypt = require('bcrypt')
 const Koa = require('koa')
 const app = new Koa()
+const { ObjectId } = require('mongodb')
 require('../mongo')(app)
 module.exports = ({router}) => {
 
   // Create a new user given username and password
   router.post('/signUp', async (ctx) => {
-    // Check to see if an existing user exists with that username
+    // Check to see if a user exists with given username
     let existingUser = await app.users.findOne({username: ctx.request.body.username})
     // If so, return message and 409 status
     if(existingUser){
       ctx.body = 'Username already exists'
       ctx.status = 409
-    }
-    // Otherwise, proceed with insert
-    else {
+    } else {
       // Use bcrypt to encrypt the password and store it with the new user
       app.users.insertOne({
         username: ctx.request.body.username,
@@ -22,6 +21,41 @@ module.exports = ({router}) => {
       })
       console.log(`Username: ${ctx.request.body.username} added`)
       ctx.body = `User Added ${ctx.request.body.username}`
+    }
+  })
+
+  // Validate user
+  router.post('/validate', async (ctx) => {
+    // Check to see if a user exists with given username
+    let existingUser = await app.users.findOne({username: ctx.request.body.username})
+    // If not, return message and 404 status
+    if(!existingUser){
+      ctx.body = 'User does not exist'
+      ctx.status = 404
+    } else {
+      // Use bcrypt to encrypt incoming password and compare to stored one
+      let enteredPassword = await bcrypt.hash(ctx.request.body.password, 10)
+      console.log(enteredPassword)
+      console.log(existingUser.password)
+      if(enteredPassword === existingUser.password){
+        ctx.body = `Welcome, ${existingUser.username}`
+      } else {
+        ctx.body = 'Password is incorrect'
+        ctx.status = 422
+      }
+    }
+  })
+
+  // Delete user
+  router.delete('/:userID', async (ctx) => {
+    let userToDelete = await app.users.findOne({_id: ObjectId(ctx.params.userID)})
+    if(!userToDelete){
+      ctx.body = 'User does not exist'
+      ctx.status = 404
+    } else {
+      app.users.deleteOne({_id: ObjectId(ctx.params.userID)})
+      console.log(`Username: ${userToDelete.username} deleted`)
+      ctx.body = `Successfully deleted user ${userToDelete.username}`
     }
   })
 
