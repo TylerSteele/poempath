34303b95a8696a21990ff058ce0fde17e29c4db0
SteeleTyler <tsteele02@wildcats.jwu.edu>
2019-01-21 19:40:51 -0500

    Build user signup API, change backend file layout, basic error logging

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,10 +1,37 @@
+const bcrypt = require('bcrypt')
 const Koa = require('koa')
-const KoaRouter = require('koa-router')
+const app = new Koa()
+require('../mongo')(app)
+module.exports = ({router}) => {
 
-router.post('/signup', async(ctx) => {
-  const username = ctx.body.username
-  const password = ctx.body.password
-})
+  // Create a new user given username and password
+  router.post('/signUp', async (ctx) => {
+    // Check to see if an existing user exists with that username
+    let existingUser = await app.users.findOne({username: ctx.request.body.username})
+    // If so, return message and 409 status
+    if(existingUser){
+      ctx.body = 'Username already exists'
+      ctx.status = 409
+    }
+    // Otherwise, proceed with insert
+    else {
+      // Use bcrypt to encrypt the password and store it with the new user
+      app.users.insertOne({
+        username: ctx.request.body.username,
+        password: await bcrypt.hash(ctx.request.body.password, 10)
+      })
+      console.log(`Username: ${ctx.request.body.username} added`)
+      ctx.body = `User Added ${ctx.request.body.username}`
+    }
+  })
 
-// Koa Router
-app.use(router.routes()).use(router.allowedMethods())
+  // Return a random user
+  router.get('/randomUser', async (ctx) => {
+    ctx.body = await app.users.aggregate([{$sample: {size: 1}}])
+  })
+
+  // Return all users
+  router.get('/allUsers', async (ctx) => {
+    ctx.body = await app.users.find()
+  })
+}
