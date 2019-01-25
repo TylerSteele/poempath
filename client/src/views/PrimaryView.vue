8392dd6e0acc1e156074c2764626edd5b77b6874
SteeleTyler <tsteele02@wildcats.jwu.edu>
2019-01-25 12:30:15 -0500

    Attempt loading user data. Cannot get the watcher to work...

--- client/src/views/PrimaryView.vue
+++ client/src/views/PrimaryView.vue
@@ -2,8 +2,9 @@
   <div>
     <p id="mainHeading">poempath</p>
     <p id="subHeading">Poetry you'll enjoy. </p>
-    <PoemDisplay v-bind:title="this.poem.title" v-bind:body="this.poem.text" v-bind:author="this.poem.author"/>
-    <DecisionButtons />
+    <PoemDisplay v-bind:title="this.poem.title" v-bind:body="this.poem.text"
+                 v-bind:author="this.poem.author"/>
+    <DecisionButtons/>
   </div>
 </template>
 
@@ -16,6 +17,7 @@
     margin: .5em;
 
   }
+
   #subHeading {
     font-style: italic;
     font-size: 1.5em;
@@ -27,15 +29,39 @@
 <script>
   import PoemDisplay from '../components/PoemDisplay'
   import DecisionButtons from '../components/DecisionButtons'
-export default {
-  name: 'PrimaryView',
-  props: {
-    poem: {}
-  },
-  components: {DecisionButtons, PoemDisplay},
-  data: function() {
-    return {
+  import {UserAPI} from "../services/api/UserAPI"
+
+  export default {
+    name: 'PrimaryView',
+    props: {
+      poem: {},
+      username: {}
+    },
+    watch: {
+      username: function (newVal) {
+        if (newVal !== '') {
+          // Make the API Request for the user
+          this.getUser()
+          }
+      },
+      deep: true,
+      immediate: true
+    },
+    components: {DecisionButtons, PoemDisplay},
+    data: function () {
+      return {
+        user: {}
+      }
+    },
+    methods: {
+      mounted() {
+
+      },
+      getUser() {
+        UserAPI.getUser(this.props.username).then((data) => {
+          this.user = data
+        })
+      }
     }
   }
-}
 </script>
