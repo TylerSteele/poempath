24c461d9792609c2fd71ffa385e30db0aeff329b
SteeleTyler <tsteele02@wildcats.jwu.edu>
2019-01-27 16:36:27 -0500

    Store poem in state and show loading overlay if state isLoading is true

--- client/src/store.js
+++ client/src/store.js
@@ -1,34 +1,48 @@
 import Vue from 'vue'
 import VueX from 'vuex'
-import {UserAPI} from "./services/api/UserAPI"
+import {UserAPI} from './services/api/UserAPI'
+import { PoetryAPI } from './services/api/PoetryAPI'
 
 Vue.use(VueX)
 
 export default new VueX.Store({
   state: {
-    loadingStatus: 'notLoading',
-    currentUser: {}
-    },
+    isLoading: false,
+    currentUser: {},
+    currentPoem: {}
+  },
   mutations: {
-    SET_LOADING_STATUS(state, status) {
-      state.loadingStatus = status
+    SET_IS_LOADING(state, status) {
+      state.isLoading = status
     },
-    SET_CURRENT_USER (state, newUser){
+    SET_CURRENT_USER(state, newUser) {
       state.currentUser = newUser
+    },
+    SET_CURRENT_POEM(state, newPoem) {
+      state.currentPoem = newPoem
     }
 
   },
   actions: {
-    async loadCurrentUser ({commit}, username) {
+    async loadCurrentUser({commit}, username) {
       if (username === '') {
         commit('SET_CURRENT_USER', {})
       } else {
-        commit('SET_LOADING_STATUS', 'loading')
+        commit('SET_IS_LOADING', true)
         let newUser = await UserAPI.getUser(username)
           .catch(err => console.log(err))
         commit('SET_CURRENT_USER', newUser)
-        commit('SET_LOADING_STATUS', 'notLoading')
+        commit('SET_IS_LOADING', false)
       }
+    },
+    async loadCurrentPoem({commit}) {
+      commit('SET_IS_LOADING', true)
+      // API returns an array of length 1 (depends on the route, may need to change
+      let newPoemArray = await PoetryAPI.getPoem()
+        .catch(err => console.log(err))
+      commit('SET_CURRENT_POEM', newPoemArray[0])
+      commit('SET_IS_LOADING', false)
+
     }
   }
 })
